3023a619770af348dfef4845ad74b4e3ede49222
evasanapradhan <evasanapradhan@gmail.com>
2022-06-28 10:19:57 +0545

    find by id updated

--- src/services/routeservices.ts
+++ src/services/routeservices.ts
@@ -1,43 +1,44 @@
 import { User } from '../entity/User';
 import {Userdata} from '../contracts/Userdata'
 import { AppDataSource } from '../srcdata/data-source';
+
 export const findAll = () => {
     const usertable = AppDataSource.manager.find(User);
     return Promise.resolve(usertable);
   };
-  
-  export const findByID = (id: number) => {
-    const user = AppDataSource.manager.find((item) => item.id == id);
-    if (user) {
-      return Promise.resolve(user);
-    }
-    return Promise.reject();
+
+   //GETBYID
+  export const findByID = async(getid: number) => {
+    // const findget = AppDataSource.getRepository(User)
+    const finduser = await AppDataSource.getRepository(User).findOneBy({
+      id: getid,
+    })
+    return Promise.resolve(finduser)
   };
 
 //POST
 export const upost = (item: Userdata) => {
-    // const usertable = AppDataSource.manager.save(Use);
+    // const usertable = AppDataSource.manager.save(Use); //when using save have to delcare 
  AppDataSource.manager.insert(User,item)  //--> existing entity , new data
   return Promise.resolve(findAll());
   };
  
   //Delete
   export const deletedata = async (deleteid: number)=>{
-    const datarepository = AppDataSource.getRepository(User)
-    const D = await datarepository.findOneBy({
+    const findobject = await AppDataSource.getRepository(User).findOneBy({
       id: deleteid,
     })
-    if (D)
-await datarepository.remove(D)
+    if (findobject)
+await AppDataSource.getRepository(User).remove(findobject)
 return Promise.resolve(findAll())
   }
 
   //update
   export const update = async(updateid:number, u:Userdata) => {
-    const datarepository = AppDataSource.getRepository(User)
-    const Updateuser = await datarepository.findOneBy({
+    const Updateuser = await AppDataSource.getRepository(User).findOneBy({
       id: updateid,
     })
+    // findByID()
     console.log(Updateuser)
     if (Updateuser)
     {
@@ -45,6 +46,6 @@ return Promise.resolve(findAll())
       Updateuser.lastName = u.lastName,
       Updateuser.age = u.age
     }
-    await datarepository.save(Updateuser)
+    await AppDataSource.getRepository(User).save(Updateuser)
     return Promise.resolve(findAll())
   }
